112e4f7782ccd39fb397fc06bd904064734d370f
Minh Ha <minh@iss-cs-lab03.local>
2023-01-25 11:43:35 -0600

    helloworld on the cluster

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+# This is our hello world example
+"""
+
+from mpi4py import MPI
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+
+name = MPI.Get_processor_name()
+
+print("Hello, World! I am process %2d of %2d an %s" % (rank,size,name))
+
+
