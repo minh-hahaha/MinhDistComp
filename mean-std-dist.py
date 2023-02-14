0bfae2f721da9fc68b249e4492ca102a81c7191a
Minh Ha <minh@iss-cs-lab03.local>
2023-02-14 11:01:24 -0600

    dist mean-std

--- mean-std-dist.py
+++ mean-std-dist.py
@@ -5,6 +5,7 @@ This is my distributed version of mean-std
 
 from mpi4py import MPI
 import numpy as np
+import stattools as st
 
 
 comm = MPI.COMM_WORLD
@@ -15,25 +16,40 @@ name = MPI.Get_processor_name()
 root = 0
 
 # each rank read the data
-sendbuff = []
 
-data = np.load("/clusterfs/unranked_data.npy")
-
-data = comm.scatter(sendbuff,root)
-
-
+data = np.load("/clusterfs/ranked_data.npy")
+# print (rank, " : ", data[rank,0])
 
 
 # each rank gets a mean on its portion of the data
+rank_mean = st.getMean(data[rank])
+# print(rank, " : " , rank_mean)
 
-# return means to the root and get population mean
+
+# return means to the root (gather) and get population mean
+recvbuff = comm.gather(rank_mean,root)
+
+if rank == root:
+    recvbuff = np.stack(recvbuff)
+    pop_mean = st.getMean(recvbuff) 
+    print("pop_mean", " : " ,pop_mean) 
+else:
+    pop_mean = None
+
 
 # broadcast result to all ranks
+mean = comm.bcast(pop_mean,root)
 
 # each rank gets a sums of squares on its portion of the data
+rank_sos = st.sumsquares(data,pop_mean)
+    
+# return sums of squares to the root (reduce) and get the sum of the sums of squares 
+res = MPI.COMM_WORLD.reduce(rank_sos,root = root)
 
-# return sums of squares to the root and get the sum of the sums of squares 
-
+if rank == root:
+    #print("sum of sum of squares : " , res)
+    std = st.stdev(res, (len(data) * len(data[0])))
+    print("std", std)
 # root can calculate the std
 
 
